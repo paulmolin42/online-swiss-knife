218fea3216ad691e8eb431663e7d52ed0f40db69
Paul Molin <paulm@theodo.fr>
2015-07-17 13:37:53 +0200

    Add remote method to get current user if authenticated

--- www/common/models/improved-user.js
+++ www/common/models/improved-user.js
@@ -1,3 +1,30 @@
+loopback = require('loopback');
+
 module.exports = function(ImprovedUser) {
 
+  ImprovedUser.me = function (next) {
+    var lbContext = loopback.getCurrentContext();
+    if (!lbContext.active || !lbContext.active.accessToken || !lbContext.active.accessToken.userId)
+      next(new Error('No current user was found'));
+    else {
+      ImprovedUser.findById(lbContext.active.accessToken.userId, function (err, user) {
+        if (err) {
+          next(err);
+        } else {
+          next(null, user)
+        }
+      });
+    }
+  };
+
+  ImprovedUser.remoteMethod('me', {
+    description: 'Returns current user if authenticated',
+    http: {
+      verb: 'get'
+    },
+    returns: {
+      arg: 'currentUser',
+      type: 'object'
+    }
+  })
 };
